920bcb8d831a750e7d307d59c833314646b2939a
DAB <dab@dab.la>
2016-12-25 10:22:00 -0600

    Updated install options

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -10,7 +10,7 @@ git submodule update --init Lightning
 
 mysql -uroot -ppassword -e 'create database lightning; create user lightning@localhost identified by "lightning"; GRANT ALL PRIVILEGES ON lightning.* to lightning@localhost with grant option;'
 
-INSTALL_PHP_DEPENDENCIES=true INSTALL_VENDOR=true INSTALL_SOURCE_DEPENDENCIES=false INSTALL_SOCIAL_SIGNIN_DEPENDENCIES=false INSTALL_DEFAULT_SOURCE=true INSTALL_CK_EDITOR=true INSTALL_CKFINDER=false INSTALL_TINYMCE=false INSTALL_ELFINDER=true INSTALL_INDEX=true INSTALL_HTACCESS=false DBHOST=localhost DBNAME=lightning DBUSER=lightning DBPASS=lightning INSTALL_DATABASE=true INSTALL_DEFAULT_IMAGES=true CREATE_ADMIN=true ADMIN_EMAIL=admin@lightning.local ADMIN_PASSWORD=lightning /var/www/demo/Lightning/install.sh
+INSTALL_PHP_DEPENDENCIES=true INSTALL_VENDOR=true INSTALL_SYSTEM_DEPENDENCIES=true INSTALL_SOURCE_DEPENDENCIES=false INSTALL_SOCIAL_SIGNIN_DEPENDENCIES=false INSTALL_DEFAULT_SOURCE=true INSTALL_CK_EDITOR=true INSTALL_CKFINDER=false INSTALL_TINYMCE=false INSTALL_ELFINDER=true INSTALL_INDEX=true INSTALL_HTACCESS=false DBHOST=localhost DBNAME=lightning DBUSER=lightning DBPASS=lightning INSTALL_DATABASE=true INSTALL_DEFAULT_IMAGES=true CREATE_ADMIN=true ADMIN_EMAIL=admin@lightning.local ADMIN_PASSWORD=lightning /var/www/demo/Lightning/install.sh
 
 mkdir /var/www/logs
 chown -R www-data:www-data /var/www/logs
